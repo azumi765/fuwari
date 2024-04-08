31447b6e6d5a5d43bc32745808f18e938a817125
ITSUKI <127605317+azumi765@users.noreply.github.com>
2024-04-08 22:56:44 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',
+  title: 'ITSUKI',
+  subtitle: 'Blog Site',
+  lang: 'ja',
   themeHue: 250,
   banner: {
     enable: false,
